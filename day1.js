48e901060f80dcba55fbf65cad9ff8e80278f550
veena172 <veenaanejaa@gmail.com>
2026-01-24 14:26:25 +0530

    Add Day 1 JavaScript Basics Revision

---
+++ day1.js
@@ -0,0 +1,15 @@
+// Day 1 – JavaScript Basics Revision
+
+let name = "Veena";
+let marks = 65;
+
+function checkResult(marks) {
+  if (marks >= 40) {
+    console.log("Pass");
+  } else {
+    console.log("Fail");
+  }
+}
+
+console.log("Name:", name);
+checkResult(marks);
