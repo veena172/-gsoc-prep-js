6dee802acf872bb6a95b542aef377db4efc622c6
veena172 <veenaanejaa@gmail.com>
2026-02-04 14:09:24 +0530

    Add Day 5 practice and create js folder structure

---
+++ js/day5.js
@@ -0,0 +1,16 @@
+// ===============================
+// Day 5 – GitHub Workflow Practice
+// ===============================
+
+// Simple practice functions
+
+function greet(name) {
+  return "Hello " + name;
+}
+
+function square(num) {
+  return num * num;
+}
+
+console.log(greet("Veena"));
+console.log(square(5));
