33bff54fe1113ea43e4dd4f24a0058674fd905c8
veena172 <veenaanejaa@gmail.com>
2026-02-07 15:37:52 +0530

    Add day6.js with greet and square functions

---
+++ js/day6.js
@@ -0,0 +1,14 @@
+// ===============================
+// Day 6 – Local Git Practice
+// ===============================
+
+function greet(name) {
+  return "Hello " + name;
+}
+
+function square(num) {
+  return num * num;
+}
+
+console.log(greet("Veena"));
+console.log(square(6));
